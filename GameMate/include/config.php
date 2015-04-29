a2aa8ea9ae702b3ea8790a7a7cb78ca1ffc9c089
Luigi Giugliano <luigi3000@gmail.com>
2015-04-29 19:57:46 +0200

    Delete config.php

--- GameMate/include/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Database config variables
- */
-define("DB_HOST", "localhost");
-define("DB_USER", "gamemate");
-define("DB_PASSWORD", "bdsir");
-define("DB_DATABASE", "my_gamemate");
-?>
